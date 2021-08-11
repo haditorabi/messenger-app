74f52de6391e55da8950a0b1c68512b3a60b5745
haditorabi <haditorabi1984@gmail.com>
2021-08-11 12:16:29 -0400

    change order of messages for test in view

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,7 +19,7 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      order: [[Message, "createdAt", "ASC"]],
       include: [
         { model: Message, order: ["createdAt", "DESC"] },
         {
