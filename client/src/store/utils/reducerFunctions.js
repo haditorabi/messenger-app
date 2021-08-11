2dbc6f35443222aca7b2c17df18df10481a11f56
haditorabi <haditorabi1984@gmail.com>
2021-08-11 12:30:20 -0400

    remove comments and console.log's

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -13,15 +13,10 @@ export const addMessageToStore = (state, payload) => {
 
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      // convo.messages.push(message);
-      // convo.latestMessageText = message.text;
-      // console.log(convo.latestMessageText)
-      // console.log(convo.messages)
-      // console.log("convoOBJ", convo)
-      const newConvo = {...convo};
-      newConvo.messages.push(message);
-      newConvo.latestMessageText = message.text;
-      return newConvo;
+      const newConvoObj = {...convo};
+      newConvoObj.messages.push(message);
+      newConvoObj.latestMessageText = message.text;
+      return newConvoObj;
 
     } else {
       return convo;
